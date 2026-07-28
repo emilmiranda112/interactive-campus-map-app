4f8e221e283b98b51d8d38053f039b5973a0515c
emilamiranda@icloud.com <emilamiranda@icloud.com>
2026-07-28 17:53:23 +0000

    Edited DataParser, Form1, Form1.ItemTemplate1, Form1.ItemTemplate2 and 1 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,9 @@
 from ._anvil_designer import Form1Template
 import anvil.server
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.js
 
 class Form1(Form1Template):
